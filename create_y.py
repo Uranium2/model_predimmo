b84308dc8a937c83c90faed903c9572673dcb710
Arastal <bergeron.florian@gmail.com>
2020-07-08 02:23:54 +0200

    [DEL] - Refactor repository 1

--- create_y.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_csv("./data/big_df.csv", encoding="utf-8")
-
-# df['y'] = df.apply(lambda row: row.valeur_fonciere_2016 - row.valeur_fonciere_2017, axis = 1) 
-
-df['y'] = (df['valeur_fonciere_2016'] / df['valeur_fonciere_2019']) * 100
-print(df)
