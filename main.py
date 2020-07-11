166f3f226f77c3a80194494f121b0371f7a87362
Arastal <bergeron.florian@gmail.com>
2020-07-12 00:00:06 +0200

    [UP] - Add total processus duration.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+import datetime
+
 from preprocessing_1year import preprocessing_1year
 from preprocessing_3years import preprocessing_3years
 from preprocessing_current_year import preprocessing_current_year
@@ -9,6 +12,7 @@ from predict_current_year import predict_current_year
 from rds import push_data_to_RDS
 
 if __name__ == "__main__":
+    start_timer = time.time()
     preprocessing_1year()
     preprocessing_3years()
     preprocessing_current_year()
@@ -18,3 +22,8 @@ if __name__ == "__main__":
     predict_classification_3years()
     predict_current_year()
     push_data_to_RDS()
+
+    print("\n#################################################################")
+    print("#####       PROCESSUS FINISHED: RDS HAS BEEN UPDATED!       #####")
+    print("#####    --- Full processus duration: %s ---    #####" % (str(datetime.timedelta(seconds = (time.time() - start_timer)))))
+    print("#################################################################\n")
